b76192c78d3f561e8aa93edd9b33fa593675d0a5
Raymond <raymondtdev@gmail.com>
2026-07-29 16:46:04 +0200

    created invoice schema

---
+++ server/src/models/InvoiceSchema.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const InvoiceSchema = new Schema({
+  project: { type: Schema.Types.ObjectId, ref: "Project", requred: true },
+  client: { type: Schema.Types.ObjectId, ref: "Client", requred: true },
+  number: { type: String, requred: true },
+  status: {
+    type: String,
+    enum: [ "pending", "paid" ],
+    default: "pending"
+  },
+  lineItems: [{
+    description: { type: String, required: true },
+    amount: { type: Number, required: true }
+  }],
+  total: { type: Number, required: true }
+});
+
+const Invoice = mongoose.model("Invoice", InvoiceSchema);
+
+export default Invoice;
