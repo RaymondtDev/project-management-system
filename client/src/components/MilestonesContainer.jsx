c61a9ea99a00dc2c8ed0ad2cc99728fb9bab3e56
Raymond <raymondtdev@gmail.com>
2026-08-19 12:28:01 +0200

    add milestone ui

--- client/src/components/MilestonesContainer.jsx
+++ client/src/components/MilestonesContainer.jsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import MilestoneCard from "./MilestoneCard";
+import { FaPlus } from "react-icons/fa";
 
 export default function MilestonesContainer({ milestonesData }) {
-  const [display, setDisplay] = useState(false);
+  const [displayInpt, setDisplayInpt] = useState(false);
 
   return (
     <div className="px-4 py-6 bg-white shadow-md rounded-md min-h-0 h-full overflow-y-auto scrollbar-thin scrollbar-thumb-primary-bg">
       <div className="flex items-center justify-between relative">
         <small className="uppercase">Milestones</small>
-        <div className="size-7 flex flex-col items-center justify-center gap-0.5 cursor-pointer rounded-full hover:bg-gray-200" onClick={() => setDisplay(!display)}>
-          <span className="size-1 rounded-full bg-black"></span>
-          <span className="size-1 rounded-full bg-black"></span>
-          <span className="size-1 rounded-full bg-black"></span>
-        </div>
-        <div className={`${!display && "hidden"} bg-gray-50 p-2 rounded-sm absolute top-[110%] right-2.5 z-40 shadow-lg`}>
-          <small>Add Milestone</small>
-        </div>
+        <button onClick={() => setDisplayInpt(!displayInpt)} className="flex items-center gap-2 p-2 bg-primary-bg text-white rounded-full cursor-pointer">
+          <FaPlus />
+        </button>
       </div>
       <div className="flex flex-col gap-2 mt-4">
+        { displayInpt && (
+          <div className="flex items-center gap-2">
+            <input type="text" placeholder="Enter milestone title" className="flex-1 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-bg" />
+            <button className="px-4 py-2 bg-primary-bg text-white rounded-md hover:bg-primary-bg/80">Add</button>
+            <button onClick={() => setDisplayInpt(false)} className="px-4 py-2 bg-gray-300 text-black rounded-md hover:bg-gray-400">Cancel</button>
+          </div>
+        )}
         {milestonesData?.map((milestone, index) => (
           <MilestoneCard
             milestone={milestone}
